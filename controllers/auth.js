d1b48b4535062e79eff338ff8d1ab4b84fe66430
Harry <h.harrison1993@gmail.com>
2020-06-12 11:49:38 +0100

    Updated file paths for heroku

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken"); //generate signed token
 const expressJwt = require("express-jwt"); //authorization check
 
--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const godSchema = require("../models/God");
 
 exports.userById = (req, res, next, id) => {
